cdc79845167c48ed8d328f4158562a8808c2217f
KaareI <karlkaareltaniel.seppel@gmail.com>
2023-11-02 09:31:43 +0200

    Undesigned prototype

---
+++ src/screens/Home/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Text, View, Image} from "react-native"
+import Button from "../../Components/Button";
+import {styles} from "./styles";
+
+const Home = ()  => {
+    return (
+        <View style={styles.container}>
+        <Image resizeMode="contain" style={styles.image} source={require('../../assets/brazil-silhouette.png')}/>
+
+        <View style={styles.titleContainer}>
+        <Text style={styles.title}>Take a guess:</Text>
+        </View>
+
+        <Button title="Brazil" />
+        <Button title="Russia" />
+        <Button title="Australia" />
+
+        </View>
+    )
+}
+export default Home;
